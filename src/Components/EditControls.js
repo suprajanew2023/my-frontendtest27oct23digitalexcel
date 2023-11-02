c6ceb81a9365bd077c70fb63d5206b6f9a5de1be
suprajanew2023 <149648986+suprajanew2023@users.noreply.github.com>
2023-11-02 10:13:14 +0530

    Update EditControls.js

--- src/Components/EditControls.js
+++ src/Components/EditControls.js
@@ -37,7 +37,7 @@ function ControlDetailsTable() {
 
   const fetchControlDetails = () => {
           if (standard) {
-            Axios.get(`https://backenddigitalexcel.onrender.com/getControlsByStandard/${standard}`)
+            Axios.get(`https://digitalexcelbackend.onrender.com/getControlsByStandard/${standard}`)
               .then((response) => {
                 setData(response.data);
               })
@@ -60,7 +60,7 @@ function ControlDetailsTable() {
       
         useEffect(() => {
           // Fetch the list of standards when the component mounts
-          Axios.get('https://backenddigitalexcel.onrender.com/standards')
+          Axios.get('https://digitalexcelbackend.onrender.com/standards')
             .then((response) => {
               setStandardsList(response.data);
             })
