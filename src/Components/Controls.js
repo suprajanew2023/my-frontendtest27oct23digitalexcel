73a90e0e8ea2c7d5321209f16510f45bacffa1e0
suprajanew2023 <149648986+suprajanew2023@users.noreply.github.com>
2023-11-09 16:30:43 +0530

    Update Controls.js

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -3,17 +3,27 @@ import EditControls from './EditControls';
 import AddControls from './AddControls';
 import './ControlPage.css';
 import AAQ from "../LoginPage/AAQ-1904.png"
-
+import { Link } from 'react-router-dom'; 
+import Button from '@mui/material/Button'; 
+import LogoutIcon from '@mui/icons-material/Logout';
 const Controls = () => {
     const [showAddControls, setShowAddControls] = useState(false);
+ 
 
+    const handleLogout = () => {
+        // window.location.href = '/'
+       
+    };
     return (
         <div className='bgconatainer'>
         <header className='header-1'>
             <div className='logo-container'>
                 <img className='rhym-logo' src={AAQ} alt="logo"/>
             </div>
-
+            <Button component={Link}  style={{ marginRight: '10px' }}  className='button-logout' onClick={handleLogout} to="/">
+            <LogoutIcon></LogoutIcon>
+                    Logout
+                </Button>
         </header>
       <div className='boom'>
       <button className='button-login control-btn' onClick={() => setShowAddControls(!showAddControls)}>
